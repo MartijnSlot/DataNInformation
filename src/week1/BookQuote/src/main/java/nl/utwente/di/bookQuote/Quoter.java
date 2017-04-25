3ca69ef2d2bce38fb82978cbe9573ab5efbaabeb
MartijnSlot <martijn.slot@nedap.com>
2017-04-25 14:12:46 +0200

    wk1webprog1§

---
+++ src/week1/BookQuote/src/main/java/nl/utwente/di/bookQuote/Quoter.java
@@ -0,0 +1,11 @@
+package nl.utwente.di.bookQuote;
+
+/**
+ * Created by martijn.slot on 25/04/2017.
+ */
+public class Quoter {
+
+    public double getBookPrice(String s){
+        return 0;
+    }
+}
